dd60e240d3e4284b6ec32ad95022209209640298
Nitin Kumar <nitinsingh.kmr@gmail.com>
2026-09-18 15:46:34 +0530

    add functionality to check if a number is prime

---
+++ level1_basics/04_is_prime.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the number: "))
+
+if n < 2:
+    print("False")
+
+else:
+    is_prime = True
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+
+    print(is_prime)
